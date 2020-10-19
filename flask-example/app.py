a5ecaeed8d7e273eb9a2e993f57b8d8aea7cfb1c
lily <swap@gmail.com>
2020-10-19 13:05:15 +0530

    Added a templates folder

--- flask-example/app.py
+++ flask-example/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello World!!"
+def hello_world(name=None):
+    return render_template("index.html", name=name)
 
 
 @app.route("/hello")
